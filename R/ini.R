922ca58b1cd27393147ddcb5bd921b794ca16dbd
tomvar <tkopczewski@gmail.com>
2017-02-02 12:00:02 +0100

    a
    a

--- R/ini.R
+++ R/ini.R
@@ -4,33 +4,31 @@ for(i in requiredPackages){
   if(!require(i,character.only = TRUE)) install.packages(i)
   library(i,character.only = TRUE) } # za³odowanie potrzebnych pakietów
 
-
-
-
-# require(foreach)
-# require(doParallel)
+#
+#  require(foreach)
+#  require(doParallel)
 #
 #
-# if(require("foreach")){
-#   print("foreach is loaded correctly")
-# } else {
-#   print("trying to install foreach")
-#   install.packages("foreach")
-#   if(require(foreach)){
-#     print("foreach installed and loaded")
-#   } else {
-#     stop("could not install foreach")
-#   }
-# }
+#  if(require("foreach")){
+#    print("foreach is loaded correctly")
+#  } else {
+#    print("trying to install foreach")
+#    install.packages("foreach")
+#    if(require(foreach)){
+#      print("foreach installed and loaded")
+#    } else {
+#      stop("could not install foreach")
+#    }
+#  }
 #
-# if(require("doParallel")){
-#   print("doParallel is loaded correctly")
-# } else {
-#   print("trying to install doParallel")
-#   install.packages("doParallel")
-#   if(require(doParallel)){
-#     print("doParallel installed and loaded")
-#   } else {
-#     stop("could not install doParallel")
-#   }
-# }
+#  if(require("doParallel")){
+#    print("doParallel is loaded correctly")
+#  } else {
+#    print("trying to install doParallel")
+#    install.packages("doParallel")
+#    if(require(doParallel)){
+#      print("doParallel installed and loaded")
+#    } else {
+#      stop("could not install doParallel")
+#    }
+#  }
